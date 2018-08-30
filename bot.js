82a8494c30acd54476ff2016282024eb34bdc792
exvient <42811870+exvient@users.noreply.github.com>
2018-08-29 21:08:30 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -69,7 +69,7 @@ bot.on("message", async message => {
     
 
     let cmd = bot.commands.get(command.slice(prefix.length))
-    if(cmd) cmd.run(bot, message, args, con)})
+    if(cmd) cmd.run(bot, message, args)})
 
 bot.login(TOKEN);
 
